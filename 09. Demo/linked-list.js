d52ed9ae03a33356b8aa343cf9ae9eae4f29149b
mashfiq-rayhan <mashfiq.rayhan.ovi@gmail.com>
2025-06-29 20:17:34 +0600

    updates

---
+++ 09. Demo/linked-list.js
@@ -0,0 +1,44 @@
+"use strict";
+class ListNode {
+    value;
+    next;
+    constructor(value) {
+        this.value = value;
+    }
+}
+class LinkedList {
+    root;
+    tail;
+    length = 0;
+    add(value) {
+        const node = new ListNode(value);
+        if (!this.root || !this.tail) {
+            this.root = node;
+            this.tail = node;
+        }
+        else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.length++;
+    }
+    getNumberOfElements() {
+        return this.length;
+    }
+    print() {
+        let current = this.root;
+        while (current) {
+            console.log(current.value);
+            current = current.next;
+        }
+    }
+}
+const numberList = new LinkedList();
+numberList.add(10);
+numberList.add(5);
+numberList.add(0);
+numberList.add(-5);
+numberList.add(-10);
+console.log(numberList.getNumberOfElements());
+numberList.print();
+const nameList = new LinkedList();
